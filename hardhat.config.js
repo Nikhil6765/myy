6ad258775dc7bccb72944742bb9404dd1f71dc9e
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-11-14 12:33:37 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,48 @@
+
+require("@nomicfoundation/hardhat-toolbox");
+
+const FORK_FUJI = false;
+const FORK_MAINNET = false;
+let forkingData = undefined;
+
+if (FORK_MAINNET) {
+  forkingData = {
+    url: "https://api.avax.network/ext/bc/C/rpcc",
+  };
+}
+if (FORK_FUJI) {
+  forkingData = {
+    url: "https://api.avax-test.network/ext/bc/C/rpc",
+  };
+}
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  etherscan: {
+    apiKey: "RWAJTTB1FEC59X345E3G4J4Z82HJZTFWA6",
+  },
+  solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      gasPrice: 225000000000,
+      chainId: !forkingData ? 43112 : undefined, //Only specify a chainId if we are not forking
+      forking: forkingData
+    },
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [
+        "e931154682ce245186493086f301338df68739862a45913ff2ea8108e88fe4db"
+      ]
+    },
+    mainnet: {
+      url: 'https://api.avax.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: [
+        "e638c93c3b4333679add21bbede308b7860490c275185e81df742b6edb89da2e"
+      ]
+    }
+  }
+}
